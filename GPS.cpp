1e9beb467d735d9f55c24f282da80c2a30aa5bb8
Paul Tran <61669853+ZealGoxix@users.noreply.github.com>
2025-10-20 19:57:28 +0000

    needs testing, just copied code from youtube video

--- GPS.cpp
+++ GPS.cpp
@@ -8,27 +8,37 @@
     static const uint32_t GPSBaud = 9600;
 
 
-  SoftwareSerial gpsSerial(TXPin, RXPin);
+  SoftwareSerial gpsSerial(TXPin, RXPin); 
   TinyGPSPlus gps;
 
   void setup() {
       Serial.begin(9600); // For debugging output
       gpsSerial.begin(9600);
+      Serial.println("GPS Start")
   }
 
     void loop() {
-      while (gpsSerial.available() > 0) {
-        if (gps.encode(gpsSerial.read())) {
+      while (gpsSerial.available()) {
+        gps.encode(gpsSerial.read())
+      }
+    }
           // Data successfully parsed, now you can access GPS information
-          if (gps.location.isValid()) {
+          if (gps.location.isUpdated()) {
+            Serial.print("Satellite Count:");
+            Serial.println(gps.satellites.values(), 6);
             Serial.print("Latitude: ");
             Serial.println(gps.location.lat(), 6);
             Serial.print("Longitude: ");
             Serial.println(gps.location.lng(), 6);
             // ... and other data like altitude, speed, date, time
+
+            Serial.print("Altitude Feet:");
+            Serial.println(gps.altitude.feet());
+            Serial.println("");
+
           } else {
             Serial.println("Location Invalid");
-          }
+          }   
         }
-      }
-    }
+  
+    
